90cb2f1ceec0ac77e65b38d3ca257073b6b835fc
Sanika <sanikajirapure04@gmail.com>
2026-05-01 20:44:46 +0530

    Step 16: Donation and Request sync logic implemented

--- backend/app/routes/donation.py
+++ backend/app/routes/donation.py
@@ -1,7 +1,12 @@
 from fastapi import APIRouter, Depends
+from fastapi import HTTPException, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from app.database import get_db
+from fastapi import APIRouter
+from app.models.models import Donation
 from app.models import models
+from app.models.models import Request
 from app.schemas.donation_schema import DonationCreate, DonationResponse
 
 router = APIRouter(prefix="/donations", tags=["Donations"])
@@ -28,3 +33,22 @@ def create_donation(donation: DonationCreate, db: Session = Depends(get_db)):
 def get_all_donations(db: Session = Depends(get_db)):
     donations = db.query(models.Donation).all()
     return donations
+
+@router.put("/donations/{donation_id}")
+def update_donation(donation_id: int, db: Session = Depends(get_db)):
+
+    donation = db.query(Donation).filter(Donation.donation_id == donation_id).first()
+
+    if donation is None:
+        raise HTTPException(status_code=404, detail="Donation not found")
+
+        # Step 3: Sync with request
+    request = db.query(Request).filter(Request.request_id == donation.request_id).first()
+
+    if request:
+        request.status = "completed"
+
+    db.commit()
+    db.refresh(donation)
+
+    return donation
